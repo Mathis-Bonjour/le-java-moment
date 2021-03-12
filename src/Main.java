00328ddf79291b1448370e94107fc1208da560d9
bonjour <bonjour@HOS2UC05.sio.fr>
2021-03-12 09:03:23 +0100

    commit 2 electric boogaloo

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		Chien Boney = new Chien (40, 12, "Boney", "bon chien");
 		Chien Cerise = new Chien (20, 14, "Cerise", "Yorkshire");
-		Rat Jerma = new Rat ();
+		Rat Jerma = new Rat (25, );
 		Etudiant Mathis = new Etudiant ("Bonjour", "Mathis", 21, "BTS SIO", Boney);
 		List<Chien> chenil = new ArrayList<Chien>();
 		chenil.add(Boney);
